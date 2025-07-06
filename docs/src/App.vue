ec073125d5fdd81ce754e1e69300c89576f0b99f
Krisda01 <s68419010016@kktech.ac.th>
2025-07-06 21:11:39 +0700

    Create App.vue

---
+++ docs/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import MyBar from './views/mybar.vue'
+</script>
+<template>
+  <header>
+    <MyBar />
+    <router-view />
+  </header>
+  <main>
+  </main>
+</template>
+<script>
+export default {
+  name : 'App',
+  components : {
+    MyBar
+  },
+};
+
+</script>
+
+<style scoped></style>
