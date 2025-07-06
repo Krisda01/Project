186a8c4f1d25d9dd09b96f5c5c953ba4059ebbad
Krisda01 <s68419010016@kktech.ac.th>
2025-07-06 21:11:14 +0700

    Create test.vue

---
+++ docs/src/views/test.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import { ref } from 'vue';
+    const awesome = ref(true)
+</script>
+
+<template>
+    <button @click="awesome = !awesome">Submit</button>
+    <h1 v-if="awesome">you did if</h1>
+    <h1 v-else>ho no</h1>
+</template>
