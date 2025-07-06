49fc6c7f10276ef2e64fead4d32992777088cbdf
Krisda01 <s68419010016@kktech.ac.th>
2025-07-06 21:11:09 +0700

    Create NotFound.vue

---
+++ docs/src/views/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>404 - Page Not Fonud</h1>
+        <p>The page you're looking for doesn't exist.</p>
+        <button @click="goHome">Go To Home</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'NotFound',
+    methods:{
+        goHome(){
+            this.$router.push('/');
+        },
+    },
+};
+</script>
+<style>
+h1{
+    color: red;
+}
+</style>
